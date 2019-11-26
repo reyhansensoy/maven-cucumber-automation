f12d212dd1e523e852b43d3a06a2516a966d6167
reyhansensoy <sensoy.reyhan@gmail.com>
2019-11-25 22:04:45 -0800

    add new code on new branch

--- src/test/java/com/vytrack/pages/Vehicle.java
+++ src/test/java/com/vytrack/pages/Vehicle.java
@@ -5,6 +5,7 @@ public class Vehicle {
     public static void main(String[] args) {
         System.out.println("new project!!");
         System.out.println("new code!!!!!!");
+        System.out.println("I created new branch which is called tester!!!");
     }
 
 
